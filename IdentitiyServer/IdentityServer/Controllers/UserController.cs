959f3aca18ac151e347a5694059eeaa6e0e22515
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-07-23 17:22:31 +0300

    Authorize process

--- IdentitiyServer/IdentityServer/Controllers/UserController.cs
+++ IdentitiyServer/IdentityServer/Controllers/UserController.cs
@@ -1,20 +1,27 @@
 ﻿using IdentityServer.Dtos;
 using IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Shared.Dtos;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
 
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
         public class Response<T>
         {
