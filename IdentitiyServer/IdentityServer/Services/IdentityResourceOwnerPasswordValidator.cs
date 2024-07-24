3f9835ce0f24cc768a28253a3b0b274c997634b1
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-07-24 09:09:00 +0300

    IdentityResourceOwnerPasswordValidator

---
+++ IdentitiyServer/IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,43 @@
+﻿using IdentityModel;
+using IdentityServer.Models;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var IsExist = await _userManager.FindByEmailAsync(context.UserName);
+
+            if(IsExist == null)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors",new List<string> { "Email veya şifreniz yanlış"});
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+
+            var passwordCheck = await _userManager.CheckPasswordAsync(IsExist, context.Password);
+
+            if(passwordCheck == false)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "Email veya şifreniz yanlış" });
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+
+            context.Result = new GrantValidationResult(IsExist.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
+        }
+
+
+    }
+}
