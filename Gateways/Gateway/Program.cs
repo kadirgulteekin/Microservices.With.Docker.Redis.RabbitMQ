d28b0b7f43924eba1cf8c8d9f31f3d8693d81cfa
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-08-23 03:03:21 +0300

    Ocelot added

--- Gateways/Gateway/Program.cs
+++ Gateways/Gateway/Program.cs
@@ -1,6 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+builder.Services.AddOcelot();
+
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
