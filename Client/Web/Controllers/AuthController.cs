31de382ef5b8c1dfe3ea4ceaf8a1240fbe7c7c0c
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-09-04 00:55:12 +0300

    Program.cs/Cookie Settings

--- Client/Web/Controllers/AuthController.cs
+++ Client/Web/Controllers/AuthController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+using Web.Services.Interfaces;
 
 namespace Web.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly IIdentityService _ıdentityService;
+
+        public AuthController(IIdentityService ıdentityService)
+        {
+            _ıdentityService = ıdentityService;
+        }
+
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInInput signInInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var response =await _ıdentityService.SignIn(signInInput);
+
+            if(!response.IsSuccessful)
+            {
+                response.Errors.ForEach(x =>
+                {
+                    ModelState.AddModelError(String.Empty, x);
+                });
+ 
+                return View();               
+            }
+            return RedirectToAction(nameof(Index), "Home");
         }
     }
 }
--- Client/Web/Models/SignInInput.cs
+++ Client/Web/Models/SignInInput.cs
@@ -4,9 +4,11 @@ namespace Web.Models
 {
     public class SignInInput
     {
+        [Required]
         [Display(Name ="Email adresiniz")]
         public string? Email { get; set; }
         [Display(Name = "şifreniz")]
+        [Required]
         public string? Password { get; set; }
         [Display(Name = "Beni hatırla")]
         public bool IsRemember { get; set; }
